1ddb57ff69c30f5e1e3d798edcf2bce625fa18ab
Jacob Schmitz <jake.schmitz101@gmail.com>
2025-06-21 13:14:37 -0400

    Update GKE cluster to use single zone with 1 node
    - Changed cluster location from us-central1 (regional) to us-central1-a (zonal)
    - Reduced node count from 3 to 1 to avoid regional multiplication
    - This reduces cost by using only 1 node instead of 9 nodes (3 per zone)

--- infra/gke.tf
+++ infra/gke.tf
@@ -1,17 +1,26 @@
 resource "google_container_cluster" "primary" {
   name     = "promptionary-gke"
-  location = var.region
+  location = "us-central1-a"
 
   network    = google_compute_network.vpc_network.id
   subnetwork = google_compute_subnetwork.subnet.id
 
-  initial_node_count = 1
   deletion_protection = false
 
   ip_allocation_policy {}
 
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "promptionary-node-pool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
   node_config {
-    machine_type = "e2-micro"
+    machine_type = "e2-small"
     disk_size_gb = 20
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
